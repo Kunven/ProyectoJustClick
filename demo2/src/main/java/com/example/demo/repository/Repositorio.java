7e3743462582390adae0c9bd18c1bb0cc23f0f69
Luis Roberto Andrade Chun <90422643+Kunven@users.noreply.github.com>
2021-09-09 16:18:45 -0500

    Delete demo2 directory

--- demo2/src/main/java/com/example/demo/repository/Repositorio.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import com.example.demo.Datos;
-
-public interface Repositorio extends ElasticsearchRepository<Datos, String> {
-    
-}
